d331a43e76757bb26382057681fd9fe2524aa526
Victor Saravia <mavicdevtech@gmail.com>
2023-11-04 22:55:48 -0500

    feat: start project

---
+++ src/test/java/com/bancom/retobackend/RetoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancom.retobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
